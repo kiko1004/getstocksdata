608ea6b5ce8ddfeb1b38addfab2556bb4c08689d
kiko1004 <kirilspiridonov@abv.bg>
2021-01-05 23:57:05 +0200

    design update

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html", string_variable="")
+    return render_template("index.html", string_variable="")
 
 
 @app.route("/get-data", methods=["GET", "POST"])
@@ -22,9 +22,9 @@ def get_data():
         except:
             data = "TICKER NOT FOUND"
 
-        return render_template("home.html", string_variable=data.split('\n'))
+        return render_template("index.html", string_variable=data.split('\n'))
 
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 
