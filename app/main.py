24cb68b9157f55da1981b8956506ecf0ef4de768
kiko1004 <kirilspiridonov@abv.bg>
2021-01-13 14:31:21 +0200

    updated as a whole

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import gsd
 from flask import Flask, render_template, make_response
 from flask import request, redirect
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -15,17 +16,34 @@ def index():
 
 @app.route("/get-data", methods=["GET", "POST"])
 def get_data():
+
     if request.method == "POST":
         try:
             req = request.form
             ticker = req.get("ticker")
             data = gsd.getStockData_string(ticker)
+            a = gsd.getA(ticker)
             # response = make_response(getKickstartFile())
             # response.headers["content-type"] = "text/plain"
+            spliteddata = data.split('\n')
+            i = 0
+            lables = []
+            values = []
+            while i<len(spliteddata):
+                if  i%2==0:
+                    lables.append(spliteddata[i])
+                else: values.append(spliteddata[i])
+                i+=1
+
+            ziper = zip(lables, values)
+            ziper = dict(ziper)
+
+
+
         except:
             data = "TICKER NOT FOUND"
 
-        return render_template("index.html", string_variable=data.split('\n'))
+        return render_template("index.html", string_variable=data.split('\n'), a_data = a, labels = lables, values=values, ziper = ziper)
 
     return render_template("index.html")
 
--- gsd.py
+++ gsd.py
@@ -3,6 +3,8 @@ import pandas as pd
 import finviz
 from flask import app
 
+def getA(ticker):
+    return finviz.get_stock(ticker)
 
 def getStockData(ticker):
     val = si.get_stats_valuation(ticker)
@@ -54,22 +56,22 @@ def getStockData_string(ticker):
     sheet = si.get_balance_sheet(ticker)
     income = si.get_income_statement(ticker)
     cash = si.get_cash_flow(ticker)
-    one = ("EPS " + str(quote["EPS (TTM)"]))
-    two = ("Market Cap " + marketCap)
-    three = ("Dividend & Yield " + str(quote["Forward Dividend & Yield"]))
-    f = ("P/E " + str(quote["PE Ratio (TTM)"]))
-    f_2 = ("Price-to-Cashflow  " + a["P/FCF"])
-    five = ("Price/Sales " + psratio)
-    six = ("Debt/Equity " + debtEquity)
-    seven = ("Return-On-Assets " + returnOnAssets)
-    eight = ("Return-On-Equity " + returnOnEquity)
-    nine = ("Gross Margin " + grossMargin)
-    ten = ("Net Income " + str(income.iloc[4, 0]))
-    eleven = ("Operating Income " + str(income.iloc[8, 0]))
-    twelve = ("Gross Profit " + str(income.iloc[6, 0]))
-    t_2 = ("Total Assets  " + str(sheet.iloc[3, 0]))
-    thirteen = ("Total Liabilities " + str(sheet.iloc[0, 0]))
-    fourteen = ("Total Shareholder Equity " + str(sheet.iloc[1, 0]))
-    fifteen = ("Net Cashflow " + str(cash.iloc[8, 0]))
+    one = ("EPS " +"\n"+ str(quote["EPS (TTM)"]))
+    two = ("Market Cap "+"\n" + marketCap)
+    three = ("Dividend & Yield "+"\n" + str(quote["Forward Dividend & Yield"]))
+    f = ("P/E "+"\n" + str(quote["PE Ratio (TTM)"]))
+    f_2 = ("Price-to-Cashflow  "+"\n" + a["P/FCF"])
+    five = ("Price/Sales "+"\n" + psratio)
+    six = ("Debt/Equity "+"\n" + debtEquity)
+    seven = ("Return-On-Assets "+"\n" + returnOnAssets)
+    eight = ("Return-On-Equity "+"\n" + returnOnEquity)
+    nine = ("Gross Margin "+"\n" + grossMargin)
+    ten = ("Net Income "+"\n" + str(income.iloc[4, 0]))
+    eleven = ("Operating Income "+"\n" + str(income.iloc[8, 0]))
+    twelve = ("Gross Profit "+"\n" + str(income.iloc[6, 0]))
+    t_2 = ("Total Assets  "+"\n" + str(sheet.iloc[3, 0]))
+    thirteen = ("Total Liabilities "+"\n" + str(sheet.iloc[0, 0]))
+    fourteen = ("Total Shareholder Equity "+"\n" + str(sheet.iloc[1, 0]))
+    fifteen = ("Net Cashflow "+"\n" + str(cash.iloc[8, 0]))
 
     return one + "\n" + two + "\n" + three + "\n" + f + "\n"+ f_2+ "\n" + five + "\n" + six + "\n" + seven + "\n" + eight + "\n" + nine + "\n" + ten + "\n" + eleven + "\n" + twelve + "\n" + t_2 + "\n" + thirteen + "\n" + fourteen + "\n" + fifteen
